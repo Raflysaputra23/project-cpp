06c1270e9c844326da1e16d61fc989a9c1b7a32e
AbdulGhodir <abdulghodir08@gmail.com>
2024-12-12 00:13:40 +0700

    gambar alien

---
+++ src/alien.cpp
@@ -0,0 +1,20 @@
+#include <ncurses/curses.h>
+#include <string>
+using namespace std;
+
+void alienShip1(){
+    string alien [2] =  "<d00b>"
+                        " ^\\/^";
+}
+
+void alienSip2(){
+    string alien [3] =  " ___"
+                        "(=_=)"
+                        " /""\\ ";
+}
+
+void alienSip3(){
+    string alien [3] =  "  \\_/"
+                        " |(*_*)|"
+                        "  -/ \\-";
+}
